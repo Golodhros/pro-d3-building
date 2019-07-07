3f1bb94d333b583b87ebd2d392357ff342d31906
Marcos Iglesias <golodhros@gmail.com>
2019-07-07 14:21:15 -0700

    Updating step 1

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const parts = require('./webpack.parts');
 const constants = require('./webpack.constants');
 
@@ -25,39 +23,7 @@ const prodBundleConfig = merge([
     parts.externals(),
 ]);
 
-const devConfig = merge([
-    {
-        mode: 'development',
-        devtool: 'cheap-eval-source-map',
-        entry: constants.DEMOS,
-        output: {
-            path: path.resolve(__dirname, 'demos/build'),
-            filename: '[name].js'
-        },
-        devServer: {
-            contentBase: './demos/build',
-            port: 8001,
-            inline: true,
-            hot: true,
-            open: true,
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                title: 'Development',
-                template: 'src/demos/index.html'
-            })
-        ],
-    },
-    parts.cssLoader(),
-    parts.babelLoader(),
-]);
-
 module.exports = (env) => {
-
-    if (env === 'dev') {
-        return devConfig;
-    }
-
     if (env === 'production') {
         return prodBundleConfig;
     }
