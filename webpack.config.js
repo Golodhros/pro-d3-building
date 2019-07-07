efea9b99589c3555c2a685008fd294cf0a0aa02b
Marcos Iglesias <golodhros@gmail.com>
2019-07-07 13:58:35 -0700

    Development mode working with unstyled demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const parts = require('./webpack.parts');
 const constants = require('./webpack.constants');
@@ -23,7 +24,38 @@ const prodBundleConfig = merge([
     parts.externals(),
 ]);
 
+const devConfig = merge([
+    {
+        mode: 'development',
+        devtool: 'cheap-eval-source-map',
+        entry: constants.DEMOS,
+        output: {
+            path: path.resolve(__dirname, 'demos/build'),
+            filename: '[name].js'
+        },
+        devServer: {
+            contentBase: './demos/build',
+            port: 8001,
+            inline: true,
+            hot: true,
+            open: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Development',
+                template: 'src/demos/index.html'
+            })
+        ],
+    },
+    parts.babelLoader(),
+]);
+
 module.exports = (env) => {
+
+    if (env === 'dev') {
+        return devConfig;
+    }
+
     if (env === 'production') {
         return prodBundleConfig;
     }
--- webpack.constants.js
+++ webpack.constants.js
@@ -1,5 +1,13 @@
 const path = require('path');
 
+exports.CHARTS = {
+    bar: './src/charts/bar.js',
+};
+
+exports.DEMOS = {
+    'demo-bar': './src/demos/demo-bar.js',
+};
+
 exports.PATHS = {
     bundleIndex: path.resolve('./src/index.js'),
 };
